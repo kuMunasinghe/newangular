6edf63ad0b193fc570e1a4e52a03da1f5c47c2d0
kuMunasinghe <kasun.munasinghe@outlook.com>
2022-03-05 08:04:21 +0530

    Angular101

---
+++ src/app/hello-world.component.ts
@@ -0,0 +1,12 @@
+import { Template } from "@angular/compiler/src/render3/r3_ast";
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-hello-world',
+    template:'<h1>{{title}}</h1>',
+    styles:[''],
+})
+
+export class HelloWorldComponent  {
+    title = 'Hello World';
+}
